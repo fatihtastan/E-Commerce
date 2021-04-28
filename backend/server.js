69d805c0f4c24924baf7c406dbae983451ea1eed
Fatih Taştan <ftastanfanus@gmail.com>
2021-04-28 17:19:23 +0300

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+
+dotenv.config();
+
+connectDB();
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Api is ruynning');
+  res.send('Api is running');
 });
-
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
-app.listen(5000, console.log('Server is running on port 5000'));
+app.use('/api/products', productRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server is running in ${process.env.NODE_ENV} mode port ${PORT}`)
+);
